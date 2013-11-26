cabdf39cb8d059a2bfea70b2a6c8494cbf1a8c54
MakerSquare Student <students@themakersquare.com>
2013-11-26 17:13:30 -0600

    Update input names for testing

--- follower_clash.rb
+++ follower_clash.rb
@@ -2,9 +2,9 @@ require 'rubygems'
 require 'bundler/setup'
 require_relative 'lib/user_comparer'
 
-user1 = UserComparer::User.new('ornellasmike')
-user2 = UserComparer::User.new('techpeace')
-user3 = UserComparer::User.new('dsnadam')
+user1 = UserComparer::User.new('dsnadam')
+user2 = UserComparer::User.new('run4kelly')
+
 puts user1.username
 puts user2.username
 puts "#{user1.username}'s follower count: #{user1.followers}"
